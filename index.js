da0c1d9f0e193164352794d41c5a7a73e1bc0c4e
Aniket Chavan <anikettchavan103@gmail.com>
2023-12-22 15:49:02 +0530

    new

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+const express = require('express');
+const app = express();
+const PORT = 4000;
 
+// Middleware to enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); // Allow specified methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.get('/home', (req, res) => {
-  // res.status(200).json('Welcome, your app is working well');
-  res.json("Hello app is working")
-})
-
+  res.json("Hello app is working");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
-// Export the Express API
-module.exports = app
+module.exports = app;
